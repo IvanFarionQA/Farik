4008c00bf8e8d2aa08b85d713c9462301c3de28c
IvanFarionQA <ivanfarionqa@gmail.com>
2022-10-13 18:41:51 +0300

    Task2

---
+++ Task2/Task2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+class Program 
+{
+     static bool IsPalindrome(string word)
+    {
+        if (word.Length <= 1)
+            return true;
+        else
+        {
+            if (word[0] != word[word.Length - 1])
+                return false;
+            else
+                return IsPalindrome(word.Substring(1, word.Length - 2));
+        }
+    }
+    public static void Main()
+    {
+        Console.WriteLine(" Check the word : ");
+        string st;
+        bool tf;
+
+        Console.WriteLine(" Input the word : ");
+        st = Console.ReadLine();
+        tf = IsPalindrome(st);
+        if (tf == true)
+        {
+            Console.WriteLine(" The word is Palindrome.");
+        }
+        else
+        {
+            Console.WriteLine(" The word is not a Palindrome.");
+        }
+    }
+}
